bdb89d0106e0cdd1a256726c3237e413db8f5cb9
RaahulKannaK <raahulkannak@gmail.com>
2026-08-29 14:56:52 +0530

    created django setup

---
+++ blood_sos/hemohub/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('donor', 'Donor'),
+        ('needer', 'Needer'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='donor'
+    )
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
